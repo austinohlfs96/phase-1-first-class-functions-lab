abda91d7cfc6428dedcc449b389a09644ab67c95
Austin Ohlfs <ohlfsam96@gmail.com>
2023-09-18 22:55:34 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (driversArray) => {
+  return driversArray.slice(0,2);
+};
+
+const returnLastTwoDrivers = (driversArray) => {
+  return driversArray.slice(-2);
+};
+
+const selectingDrivers =
+  [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(multiply) {
+    return fare =(value) => {
+      return value * multiply
+    }
+  }
+
+const fareDoubler = (value) => {
+  return value * 2
+  } 
+
+
+  const fareTripler = (value) => {
+   return value * 3;
+    }
+  
+
+
+    const selectDifferentDrivers =(arrayOfDrivers, driverSelectorFunction) => {
+      return driverSelectorFunction(arrayOfDrivers);
+    }
